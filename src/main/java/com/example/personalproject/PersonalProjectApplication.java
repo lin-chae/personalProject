05a3d290141281f68fb1cde603f6eafc530afee2
Chae <oumuamua121@gmail.com>
2022-12-26 22:03:59 +0900

    테스트

--- src/main/java/com/example/personalproject/PersonalProjectApplication.java
+++ src/main/java/com/example/personalproject/PersonalProjectApplication.java
@@ -2,7 +2,7 @@ package com.example.personalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 테스트를 위함
 @SpringBootApplication
 public class PersonalProjectApplication {
 
