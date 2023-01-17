afb62f89f753b242358782c47f560b45b577a8e8
Chae <oumuamua121@gmail.com>
2023-01-17 23:46:44 +0900

    회원 리스트 출력

--- src/main/java/com/example/personalproject/admin/mapper/MemberMapper.java
+++ src/main/java/com/example/personalproject/admin/mapper/MemberMapper.java
@@ -1,5 +1,6 @@
 package com.example.personalproject.admin.mapper;
 
+
 import com.example.personalproject.admin.model.MemberParam;
 import com.example.personalproject.model.dto.MemberDto;
 import java.util.List;
@@ -7,7 +8,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-
 	long selectListCount(MemberParam parameter);
 
 	List<MemberDto> selectList(MemberParam parameter);
